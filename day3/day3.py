2a0e52503f7227424e5d4c07d41e882f2adaad40
Andrew Reddish <areddish@foosion.net>
2020-12-03 00:41:01 -0500

    Fixed bug with \n and didn't create more data

--- day3/day3.py
+++ day3/day3.py
@@ -1,13 +1,8 @@
 with open("day3.txt", "rt") as file:
-    data = file.readlines()
+    data = [x.strip() for x in file.readlines()]
 
     width = len(data[0])
     height = len(data)
-
-    print (width,height)
-    board = []
-    for d in range(height):
-        board.append(data[d].strip() * 8 * 15)
 
     tree_counts = []
     slopes = [(3,1),(1,1),(5,1),(7,1),(1,2)]
@@ -17,7 +12,7 @@ with open("day3.txt", "rt") as file:
         while y < height - offset[1]:
             x += offset[0]
             y += offset[1]
-            if board[y][x] == '#':
+            if data[y][x % width] == '#':
                 trees += 1
 
         tree_counts.append(trees)
@@ -28,15 +23,6 @@ with open("day3.txt", "rt") as file:
 
     print("Part 1:",tree_counts[0])
     print("Part 2:",product)
-    # print (width, height)
-    # trees = 0
-    # start = (0,0)
-    # while start[1] < len(data)-1:        
-    #     start = (move(start[0], width), start[1] + 1)        
-    #     print (start)
-    #     print(data[start[1]][start[0]])
-    #     if (data[start[1]][start[0]] == '#'):
-    #         trees += 1
         
 
     
