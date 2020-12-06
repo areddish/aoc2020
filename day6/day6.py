16b047774c1c3a41fe0e055b5c88fca1434a5e59
Andrew Reddish <areddish@foosion.net>
2020-12-06 10:21:05 -0500

    Split input into groups during read

--- day6/day6.py
+++ day6/day6.py
@@ -1,24 +1,19 @@
 #with open("test.txt", "rt") as file:
 with open("day6.txt", "rt") as file:
-    data = file.read().splitlines()
+    groups = [x.split("\n") for x in file.read().split("\n\n")]
 
     people_answered = 0
     people_common = 0
 
-    accumulate = set()
-    common = None
-    for line in data:
-        if line == "":
-            people_answered += len(accumulate)
-            people_common += len(common)
-            accumulate = set()
-            common = None
-        else:
-            accumulate |= set(line)
-            if common != None:
-                common = common.intersection(set(line))
-            else:
-                common = set(line)
+    for group in groups:
+        accumulate = set(group[0])
+        common = set(group[0])
+        for person in group[1:]:
+            accumulate |= set(person)
+            common = common.intersection(set(person))
+
+        people_answered += len(accumulate)
+        people_common += len(common)
     
     print("Part 1", people_answered)
     print("Part 2", people_common)
