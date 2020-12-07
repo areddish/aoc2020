6b6cc94a2d92a5f37c386f3f0ef186747884ec45
Andrew Reddish <areddish@foosion.net>
2020-12-07 00:42:54 -0500

    Day 7 solution

---
+++ day7/day7.py
@@ -0,0 +1,58 @@
+def can_hold_shiny(c, mapping):
+    for holds in mapping[c]:
+            if holds[1] == "shiny gold":
+                return True
+
+            if can_hold_shiny(holds[1], mapping):
+                return True
+    return False
+
+def bags_in(color, mapping):
+    if mapping[color] == []:
+        return 0
+    count = 0
+    for bags in mapping[color]:
+        count += bags[0] + bags[0] * bags_in(bags[1], mapping)
+    return count
+
+#with open("test.txt", "rt") as file:
+#with open("test2.txt", "rt") as file:
+with open("day7.txt", "rt") as file:
+    data = file.read().splitlines()
+
+    mapping = {}
+    for line in data:
+        parts = line.split(" ")
+        color = parts[0] + " " + parts[1]
+        assert parts[2] == "bags"
+        assert parts[3] == "contain"
+        x = 4
+        bags_contained = []
+        if parts[x] != "no":
+            while x < len(parts):
+                count = int(parts[x])
+                bag = parts[x+1] + " " + parts[x+2]
+                # print (f"{count} {bag}")
+                bags_contained.append((count, bag))
+                x += 4
+        # if parts[4] == no this will be empty
+        mapping[color] = bags_contained
+
+    can_hold = 0
+    for bag_color in mapping:
+        if (can_hold_shiny(bag_color, mapping)):
+            can_hold += 1
+
+    print ("Part 1", can_hold)
+
+    bag_count = 0
+    stack = ["shiny gold"]
+    while len(stack) > 0:
+        for bag_info in mapping[stack.pop()]:
+            bag_count += bag_info[0] + bag_info[0] * bags_in(bag_info[1], mapping)
+    
+    print ("Part 2", bag_count)
+
+        
+        
+
