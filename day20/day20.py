4a64e830cd3c5344124b3ca92a997b5b03747585
Andrew Reddish <areddish@foosion.net>
2020-12-24 02:20:29 -0500

    Day 20 complete

--- day20/day20.py
+++ day20/day20.py
@@ -1,4 +1,3 @@
-import copy
 import math
 from PIL import Image
 
@@ -6,6 +5,20 @@ RIGHT = 1
 BOTTOM = 2
 LEFT = 3
 TOP = 4
+
+def write_image(image, tiles, ids):
+    y_max = len(tiles[ids[0]].grid)
+    assert y_max == 10
+    # Skip first and last row
+    for y in range(1,y_max-1):
+        row = []            
+        for i in ids:
+            tile = tiles[i]
+            # Skip first and last col
+            assert len(tile.grid[y]) == 10
+            row += tile.grid[y][1:-1]                
+        image.append(row)
+
 
 def flip(data, horiztonal=True):
     data_copy = []
@@ -27,11 +40,6 @@ class Tile:
     def __init__(self, id, data):
         self.grid = data
         self.id = id
-        self.fixed = False
-        self.rotation_locked = False
-        self.manip = 0
-        self.match = [0,0,0,0,0]
-        self.i = 0
 
     def edge_number(self, which, reverse=False):
         if which == TOP:
@@ -50,7 +58,7 @@ class Tile:
 
         return sum([(2 << i) if ch == "#" else 0 for i, ch in enumerate(edge)])
 
-    def print_edges(self):
+    def find_edges(self):
         top = self.grid[0][:]
         bottom = list(reversed(self.grid[-1][:]))
         left = []
@@ -68,10 +76,8 @@ class Tile:
         return self.id, t, b, l, r
 
     def orient(self, connected_id, dir):
-        # If we are oriented, ignore
-        # flip_status = 0
-        _, t, b, l, r = self.print_edges()            
-        # Top is opposite orientaion from bottom
+        _, t, b, l, r = self.find_edges()            
+        # Top is opposite orientation from bottom
         # ---> Top
         # <--- bottom
         # if connected_id == t[1]:
@@ -90,202 +96,25 @@ class Tile:
             else:
                 assert connected_id == t[0] or connected_id == b[0]
                 self.flip_horizontal()
-                
 
         # Now rotate into position
         while connected_id != self.edge_number(dir, reverse=True):
             self.rot()
-                # if flip_status == 0:
-                #     self.flip_horizontal()
-                #     flip_status += 1
-                # else:
-                #     self.flip_vertical()
-            
-    def flip_chain(self, tiles, flipped={}, horiz=True, ):
-        if len(flipped) > 0:
-            flipped.add(self.id)
-            if horiz:
-                self.flip_horizontal()
-            else:
-                self.flip_vertical()
-            for x in self.match:
-                if x == 0 or x in flipped:
-                    continue
-                tiles[x].flip_chain(tiles, flipped, horiz)
-        else:
-            # If we are matched on the left or right we flip horiz
-            if self.match[LEFT]:
-                if self.match[RIGHT] or self.match[TOP] or self.match[BOTTOM]:
-                    return
-                self.fixed = False
-                self.i += 1
-                if self.i % 2 == 1:
-                    self.flip_vertical()
-                else:
-                    self.flip_horizontal()
-                tiles[self.match[LEFT]].flip_chain(tiles, {self.id}, horiz=True)
-            elif self.match[RIGHT]:
-                if self.match[LEFT] or self.match[TOP] or self.match[BOTTOM]:
-                    return
-                self.fixed = False
-                self.i += 1
-                if self.i % 2 == 1:
-                    self.flip_vertical()
-                else:
-                    self.flip_horizontal()
-                tiles[self.match[RIGHT]].flip_chain(tiles, {self.id}, horiz=True)
-            elif self.match[TOP]:
-                if self.match[BOTTOM] or self.match[LEFT] or self.match[RIGHT]:
-                    return
-                self.fixed = False
-                self.i += 1
-                if self.i % 2 == 1:
-                    self.flip_horizontal()
-                else:
-                    self.flip_vertical()
-                tiles[self.match[TOP]].flip_chain(tiles, {self.id}, horiz=False)
-            elif self.match[BOTTOM]:
-                if self.match[TOP] or self.match[LEFT] or self.match[RIGHT]:
-                    return
-                self.fixed = False
-                self.i += 1
-                if self.i % 2 == 1:
-                    self.flip_horizontal()
-                else:
-                    self.flip_vertical()
-                tiles[self.match[BOTTOM]].flip_chain(tiles, {self.id}, horiz=False)
 
     def flip_horizontal(self):
-        if self.fixed:
-            return
-        data = []
-        for y in self.grid:
-            row = []
-            for x in y:
-                row.insert(0,x)
-            data.append(row)
-        self.grid = data
+        self.grid = flip(self.grid, horiztonal=True)
 
     def flip_vertical(self):
-        if self.fixed:
-            return
-
-        data = []
-        for y in self.grid:
-            data.insert(0,y)
-        self.grid = data
+        self.grid = flip(self.grid, horiztonal=False)
 
     def rot(self):
-        if self.rotation_locked:
-            return
-
-        self.grid = list(zip(*self.grid[::-1]))
-        # data = copy.deepcopy(self.grid)
-
-        # for y in range(len(self.grid)):
-        #     for x in range(len(self.grid[y])):
-        #         data[y][x] = self.grid[x][-y]
-        # self.grid = data
-
-    def match_right(self, tile):
-        match = True
-        for y in range(len(self.grid)):
-            if self.grid[y][-1] != tile.grid[y][0]:
-                match = False
-                break
-        if match:            
-            self.match[RIGHT] = tile.id
-            self.rotation_locked = True
-        return match
-
-    def match_left(self, tile):
-        match = True
-        for y in range(len(self.grid)):
-            if self.grid[y][0] != tile.grid[y][-1]:
-                match = False
-                break
-        if match:
-            self.match[LEFT] = tile.id
-            self.rotation_locked = True
-        return match
-
-    def match_top(self, tile):
-        match = True
-        for x in range(len(self.grid[0])):
-            if self.grid[0][x] != tile.grid[-1][x]:
-                match = False
-                break
-        if match:
-            self.match[TOP] = tile.id
-            self.rotation_locked = True
-        return match
-
-    def match_bottom(self, tile):
-        match = True
-        for x in range(len(self.grid[0])):
-            if self.grid[-1][x] != tile.grid[0][x]:
-                match = False
-                break
-
-        if match:
-            self.match[BOTTOM] = tile.id
-            self.rotation_locked = True
-        return match
+        self.grid = rotate(self.grid)
 
     def print(self):
         for y in range(len(self.grid)):
             for x in range(len(self.grid[y])):
                 print(self.grid[y][x], end="")
             print()
-
-    def manipulate(self):
-        if self.fixed:
-            return
-
-        if self.manip == 0:
-            self.manip = 1
-        elif self.manip == 1:
-            self.rot()
-            self.manip = 2
-        elif self.manip == 2:
-            self.rot()
-            self.manip = 3
-        elif self.manip == 3:
-            self.rot()
-            self.manip = 4
-        elif self.manip == 4:
-            self.rot()
-            self.manip = 5
-        elif self.manip == 5:
-            self.flip_horizontal()
-            self.manip = 6
-        elif self.manip == 6:
-            self.rot()
-            self.manip = 7
-        elif self.manip == 7:
-            self.rot()
-            self.manip = 8
-        elif self.manip == 8:
-            self.rot()
-            self.manip = 9
-        elif self.manip == 9:
-            self.rot()
-            self.manip = 10
-        elif self.manip == 10:
-            self.flip_vertical()
-            self.manip = 11
-        elif self.manip == 11:
-            self.rot()
-            self.manip = 12
-        elif self.manip == 12:
-            self.rot()
-            self.manip = 13
-        elif self.manip == 13:
-            self.rot()
-            self.manip = 14
-        elif self.manip == 14:
-            self.rot()
-            self.manip = 15          
 
 #with open("test.txt") as file:
 with open("day20.txt") as file:
@@ -310,7 +139,7 @@ with open("day20.txt") as file:
     print(len(tiles))
     nodes = {}
     for tile in tiles:
-        id, t, b, l, r = tiles[tile].print_edges()
+        id, t, b, l, r = tiles[tile].find_edges()
         nodes[t[0]] = nodes.get(t[0], []) + [tiles[tile].id]
         nodes[b[0]] = nodes.get(b[0], []) + [tiles[tile].id]
         nodes[l[0]] = nodes.get(l[0], []) + [tiles[tile].id]
@@ -327,7 +156,7 @@ with open("day20.txt") as file:
 
     corners = []
     for tile in tiles:
-        id, t, b, l, r = tiles[tile].print_edges()
+        id, t, b, l, r = tiles[tile].find_edges()
         if t[0] not in paired_nodes and t[1] not in paired_nodes and l[0] not in paired_nodes and l[1] not in paired_nodes:
             print("upper left is ", id)
             corners.append(id)
@@ -343,13 +172,6 @@ with open("day20.txt") as file:
 
     print("Part 1", corners[0] * corners[1] * corners[2] * corners[3])
 
-    def find_tile_with_edges(tiles, ids, dir):
-        for t in tiles:
-            other_ids = tiles[t].edge_number(dir)
-            if ids[0] in other_ids or ids[1] in other_ids:
-                return tiles[t]
-        assert "Not Found"
-    image = []
     upper_left = None
     # Start in the upper left corner
     for c in corners:
@@ -362,19 +184,6 @@ with open("day20.txt") as file:
 
     current = upper_left
     width = math.sqrt(len(tiles))
-
-    def write_image(image, tiles, ids):
-        y_max = len(tiles[ids[0]].grid)
-        assert y_max == 10
-        # Skip first and last row
-        for y in range(1,y_max-1):
-            row = []            
-            for i in ids:
-                tile = tiles[i]
-                # Skip first and last col
-                assert len(tile.grid[y]) == 10
-                row += tile.grid[y][1:-1]                
-            image.append(row)
 
     image = []
     y = 0
@@ -397,43 +206,50 @@ with open("day20.txt") as file:
         print(" " + str(current.id) +" ", end="")
         while pieces < width - 1:
             ids.append(current.id)
-            #orient_piece(start)??
+
             edge_id = tiles[current.id].edge_number(direction)
             next_tile_id = nodes[edge_id][0] if nodes[edge_id][0] != current.id else nodes[edge_id][1]
             next_tile = tiles[next_tile_id]
-            # todo: orient tile
             next_tile.orient(edge_id, LEFT if direction == RIGHT else RIGHT)
 
             print(" " + str(next_tile.id) +" ", end="")
             #write_image(piece, image)
-
-
             current = next_tile
             pieces += 1
         print()
+
         ids.append(current.id)
         write_image(image, tiles, ids)
         y += 1
-        # if direction == BOTTOM and pieces :
-        #     direction = RIGHT
-        # elif pieces % (width - 1)== 0:
-        #     print()
-        #     current = upper_left
-        #     direction = BOTTOM
-        # else:
-        # #     start = get_next_piece(start, direction)
-        #     pass
-        
-    #image = flip(image, horiztonal=False)
-    # TODO multiplier
-    im = Image.new(mode="RGB", size = (len(image[0]), len(image)))
-    pix = im.load()
-    for y in range(len(image)):
-        for x in range(len(image[0])):
-            print(image[y][x], end="")
-            pix[x,y] = (255,0,0) if image[y][x] == "#" else (0,0,0)
-        print()
-    im.show()
+
+    # im = Image.new(mode="RGB", size = (len(image[0]), len(image)))
+    # pix = im.load()
+    # for y in range(len(image)):
+    #     for x in range(len(image[0])):
+    #         print(image[y][x], end="")
+    #         pix[x,y] = (255,0,0) if image[y][x] == "#" else (0,0,0)
+    #     print()
+    # im.show()
+
+    frames = []
+    def addFrame(image, mag=4):
+        im = Image.new(mode="RGB", size = (len(image[0])*mag, len(image)*mag))
+        pix = im.load()
+        for y in range(len(image)):
+            for x in range(len(image[0])):
+                color = (11,16,87)
+                if image[y][x] == "#":
+                    color= (72,47,228)
+                elif image[y][x] == "O":
+                    color = (255,255,0)
+                elif image[y][x] == "x":
+                    color = (181,230,29)
+                for wx in range(mag):
+                    for wy in range(mag):
+                        pix[(x*mag)+wx,(y*mag)+wy] = color
+        return im
+    frames.append(addFrame(image))
+
 
     # find seamonsters
     #if none found, flip horiz
@@ -462,25 +278,19 @@ with open("day20.txt") as file:
                     for offset in seamonster_offset:
                         dx,dy = offset
                         image[dy+y][x+dx] = "O"
-        
+                    frame = addFrame(image)
+                    frames.append(frame)
+                    frames.append(frame)
+                    frames.append(frame)
         if seamonsters_found == 0:
             image = op[op_index](image)
             op_index+=1
+        frame = addFrame(image)
+        frames.append(frame)
+        frames.append(frame)
+        frames.append(frame)
 
-    im = Image.new(mode="RGB", size = (len(image[0]), len(image)))
-    pix = im.load()
-    for y in range(len(image)):
-        for x in range(len(image[0])):
-            print(image[y][x], end="")
-            color = (0,0,0)
-            if image[y][x] == "#":
-                color= (255,0,0)
-            elif image[y][x] == "O":
-                color = (255,255,0)
-            pix[x,y] = color
-        print()
-    im.show()
-
+    frames[0].save("test.gif", format="GIF", append_images=frames[1:], save_all=True, duration=50, loop=0, optimize=True, quality=10)
 
     count = 0
     for y in range(len(image)):
@@ -488,91 +298,3 @@ with open("day20.txt") as file:
             if image[y][x] == "#":
                 count += 1
     print ("part 2", count)
-
-    def test_match(tiles, i, j):
-        t1 = tiles[i]
-        t2 = tiles[j]
-
-        matches = None
-        if t1.match_right(t2):
-            matches = RIGHT
-        if t1.match_bottom(t2):
-            matches = BOTTOM
-        if t1.match_left(t2):
-            matches = LEFT
-        if t1.match_top(t2):
-            matches = TOP
-
-        return matches
-
-# find upper left
-    # ops
-    # plain
-    # flip_h
-    # flip_v
-
-    count_fixed = 0
-    while count_fixed < len(tiles):
-        count_fixed = 0
-        for i in range(len(tiles)):
-            if tiles[i].fixed:
-                count_fixed += 1
-            tiles[i].manipulate()
-            for j in range(len(tiles)):
-                if i == j:
-                    continue
-                # if tiles[i].fixed and tiles[j].fixed:
-                #     continue                
-                if test_match(tiles, i, j):
-                    print('candidates', i, j, test_match(tiles, i, j), tiles[i].id)
-                    tiles[i].fixed = True
-                    tiles[j].fixed = True
-            
-        print(count_fixed)
-
-    c1 = list(filter(lambda t: t.match[TOP] == 0 and t.match[LEFT] == 0 and t.match[RIGHT] != 0 and t.match[BOTTOM] !=0, tiles))
-    c2 = list(filter(lambda t: t.match[TOP] == 0 and t.match[RIGHT] == 0 and t.match[LEFT] != 0 and t.match[BOTTOM] !=0, tiles))
-    c3 = list(filter(lambda t: t.match[BOTTOM] == 0 and t.match[LEFT] == 0 and t.match[RIGHT] != 0 and t.match[TOP] !=0, tiles))
-    c4 = list(filter(lambda t: t.match[BOTTOM] == 0 and t.match[RIGHT] == 0 and t.match[LEFT] != 0 and t.match[TOP] !=0, tiles))
-
-#     # PRINT GRID
-    tile_map = {t.id: t for t in tiles}
-#     assert c1
-#     printed = set()
-#     t = c1[0]
-# #    while len(printed) < len(tiles):
-#     printed.add(t.id)
-#     print(t.id, "-", end="")
-#     while t.match[RIGHT]:
-#         t = tile_map[t.match[RIGHT]]
-#         printed.add(t.id)
-#         print(t.id, "-", end="")
-#     print()
-
-#     exit(-1)
-
-    # We've matched rotations
-    # now we need to try flipping
-
-    while not c1 or not c2 or not c3 or not c4:
-        for i in range(len(tiles)):
-            tiles[i].flip_horizontal()
-            for j in range(len(tiles)):
-                if i == j:
-                    continue
-                if test_match(tiles, i, j):
-                    print("matched", i, j, tiles[i].id, tiles[j].id)
-                    exit(-1)
-
-        c1 = list(filter(lambda t: t.match[TOP] == 0 and t.match[LEFT] == 0 and t.match[RIGHT] != 0 and t.match[BOTTOM] !=0, tiles))
-        c2 = list(filter(lambda t: t.match[TOP] == 0 and t.match[RIGHT] == 0 and t.match[LEFT] != 0 and t.match[BOTTOM] !=0, tiles))
-        c3 = list(filter(lambda t: t.match[BOTTOM] == 0 and t.match[LEFT] == 0 and t.match[RIGHT] != 0 and t.match[TOP] !=0, tiles))
-        c4 = list(filter(lambda t: t.match[BOTTOM] == 0 and t.match[RIGHT] == 0 and t.match[LEFT] != 0 and t.match[TOP] !=0, tiles))
-
-    c1 = c1[0] if c1 else Tile(-1,[])
-    c2 = c2[0] if c2 else Tile(-1,[])
-    c3 = c3[0] if c3 else Tile(-1,[])
-    c4 = c4[0] if c4 else Tile(-1,[])
-    print(c1.id, c2.id)
-    print(c3.id, c4.id)
-    print(c1.id * c2.id * c3.id * c4.id)
